59b1801954a997c5135ad8c04647ec0460cef5c6
Nathan Reis <nathansilvareis@gmail.com>
2020-03-10 08:55:36 -0300

    Gerando minifile correto

--- src/ftt/ec/lex.java
+++ src/ftt/ec/lex.java
@@ -1,52 +1,52 @@
 package ftt.ec;
 
-import java.io.BufferedReader;
-// import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class lex {
-
-	
-	
 	public static void main(String[] args) {
-		String teste = "/*oi*/";
-		
-		System.out.println("oi" + teste.replaceAll("^[/*]*[*/]", ""));
-		/*String file ="//home/fito/eclipse-workspace/DOJO-Lex-2020/src/ftt/ec//jquery-3.4.1.js";
-		try {
-			
-		BufferedReader reader;
-		reader = new BufferedReader(new FileReader(file));
-		String line = ""
-		FileWriter fw = new FileWriter("//home//fito//silas.txt");
-		boolean x = false;
-		String completa;
-		while((line = reader.readLine()) != null) { 
-			line = line.trim();
-			System.out.println(line);
-		
-			
-			line.replaceAll("\\s+", "");
-			completa+=line;	
+		String arquivo = "//home/fito/eclipse-workspace/DOJO-Lex-2020/src/ftt/ec//jquery-3.4.1.js";
+		String conteudo = "";
+        
+    try {
+      try(FileReader fr = new FileReader(arquivo)){
+        try(BufferedReader br = new BufferedReader(fr)){
+          String linha;
 
-			
-		}
-		String regex = "^\/\*\\/$";
-		line.matches(regex) ;
-		reader.close();		
-		fw.close();
-
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
+          while((linha = br.readLine()) != null) {
+            // Remove comentários de uma linha
+            conteudo += linha.replaceAll("[\\s]*(//[\\s]+.*)|(//$)", "").replaceAll("\\s+", " ");
+          }
+        }
+      }
+            
+      try(FileWriter fw = new FileWriter(arquivo.substring(0, arquivo.lastIndexOf(".")) + ".min" + arquivo.substring(arquivo.lastIndexOf(".")))){
+        try(PrintWriter pw = new PrintWriter(fw)){
+          pw.print(conteudo.replaceAll("/\\*[^*]*(?:\\*(?!/)[^*]*)*\\*/", "") // Remove comentários de multiplas linhas
+                           .replaceAll("\\s?\\[\\s?", "\\[") // Romeve espaços entre [
+                           .replaceAll("\\s?\\]\\s?", "\\]") // Romeve espaços entre ]
+                           .replaceAll("\\s?\\(\\s?", "\\(") // Romeve espaços entre (
+                           .replaceAll("\\s?\\)\\s?", "\\)") // Romeve espaços entre )
+                           .replaceAll("\\s?\\{\\s?", "\\{") // Romeve espaços entre {
+                           .replaceAll("\\s?\\}\\s?", "\\}") // Romeve espaços entre }
+                           .replaceAll("\\s?=\\s?", "=") // Romeve espaços entre =
+                           .replaceAll("\\s?!\\s?", "!") // Romeve espaços entre !
+                           .replaceAll("\\s?\\|\\s?", "\\|") // Romeve espaços entre |
+                           .replaceAll("\\s?&\\s?", "&") // Romeve espaços entre &
+                           .replaceAll("\\s?,\\s?", ",") // Romeve espaços entre ,
+                           .replaceAll("\\s?;\\s?", ";") // Romeve espaços entre ;
+                           .replaceAll("\\s?\\:\\s?", "\\:") // Romeve espaços entre :
+                           .replaceAll("\\s?\\?\\s?", "\\?") // Romeve espaços entre ?
+                           .replaceAll("\\s?<\\s?", "<") // Romeve espaços entre <
+                           .replaceAll("\\s?>\\s?", ">")); // Romeve espaços entre >
+        }
+      }
+    } catch (Exception erro) {
+      System.out.println(erro.getMessage());
+    }
 		
-		
-		System.out.println("LEX Dojo...");*/
-		
+		System.out.println("LEX Dojo...");
 	}
-
 }
